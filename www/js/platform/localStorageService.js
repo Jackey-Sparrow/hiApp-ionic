b4dcef89e0b4b6b9998fc6bc74857a89790c2c7c
Jackey <jackey.li@foxmail.com>
2015-08-07 22:39:01 +0800

    add localStroageService

---
+++ www/js/platform/localStorageService.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Jackey Li on 2015/8/7.
+ */
+(function (angular) {
+    'use strict';
+    angular.module('ionic.util', []).
+        factory('localStorageService',
+        ['$window',
+            function ($window) {
+                return {
+                    set: function (key, value) {
+                        $window.localStorage[key] = value;
+                    },
+                    get: function (key, defaultValue) {
+                        return $window.localStorage[key] || defaultValue;
+                    },
+                    setObj: function (key, value) {
+                        $window.localStorage[key] = JSON.stringify(value);
+                    },
+                    getObj: function (key) {
+                        return JSON.parse($window.localStorage[key]) || [];
+                    }
+                }
+            }]);
+})(angular);
