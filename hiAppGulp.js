931f9a94ed791ef9ffde93ef07930d3f06cab3d5
Jackey <jackey.li@foxmail.com>
2015-08-10 23:16:11 +0800

    add hiAppGulp & upload some demo

---
+++ hiAppGulp.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Jackey Li on 2015/8/10.
+ */
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    jshint = require('gulp-jshint');
+
+//sass的编译 gulp-ruby-sass
+//自动添加css前缀 gulp-autoprefixer
+//压缩css  gulp-minify-css
+//js代码校验 gulp-jshint
+//合并js代码 gulp-concat
+//压缩js代码 gulp-uglify
+//压缩图片 gulp-imagemin
+//自动刷新页面 gulp-livereload
+//图片缓存，只有图片替换了才压缩gulp-cache
+//更改提醒 gulp-notify
+//清除文件 del
+
+
+//use jshint
+gulp.task('jshint', function () {
+    return gulp.src('www/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+//use compress
+gulp.task('compressJs', function () {
+    return gulp.src('www/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dest'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src(['dest/*.js'], {read: false})
+        .pipe(clean({force: true}));
+});
+
+//gulp.task('default', ['clean','compressJs']);
+//gulp.task('default', ['jshint']);
