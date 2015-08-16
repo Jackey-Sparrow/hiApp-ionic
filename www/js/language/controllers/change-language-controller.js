ea331eee5e0fb945f157e98a304d6b60a4c297c1
Jackey <jackey.li@foxmail.com>
2015-08-16 12:14:31 +0800

    implement change lanuage function

--- www/js/language/controllers/change-language-controller.js
+++ www/js/language/controllers/change-language-controller.js
@@ -8,8 +8,43 @@
      * change language controller
      */
     angular.module(globalSettings.appName).controller('changeLanguageController',
-        ['$scope', '$state',
-            function ($scope, $state) {
+        ['$scope', '$state', 'languageService', 'localStorageService', '$translate',
+            function ($scope, $state, languageService, localStorageService, $translate) {
+
+                //get language from language service
+                $scope.languages = languageService.getList();
+                //$scope.languageId = $scope.languages[0].LanguageId;
+
+                //get localStorage userInfo
+                var user = localStorageService.getUserInfo();
+                if (!user) {
+                    $state.go('login');
+                }
+
+
+                //$scope.chooseLanguage = languageService.getLanguageById(languageId);
+
+                $scope.changeLanguage = function (languageId) {
+                    $scope.chooseLanguage = languageService.getLanguageById(languageId);
+                    $translate.use($scope.chooseLanguage.languageTranslate);
+
+                    //modify store userInfo
+                    var language = $scope.chooseLanguage;
+                    user.languageId = language.LanguageId;
+                    user.language = language.language;
+                    user.languageName = language.languageName;
+                    user.languageTranslate = language.languageTranslate;
+                    localStorageService.setUserInfo(user);
+                };
+
+                $scope.done = function () {
+                    window.location.reload(true);
+                    $state.go('tab.setting');
+                };
+
+                /*
+                 * go back
+                 */
                 $scope.back = function () {
                     $state.go('tab.setting');
                 };
--- www/js/language/services/language-service.js
+++ www/js/language/services/language-service.js
@@ -8,55 +8,66 @@
      * language service
      */
     angular.module(globalSettings.appName).factory('languageService',
-        ['$translate', function ($translate) {
-            var service = {},
-                languageList = [
-                    {
-                        LanguageId: 1,
-                        LanguageName: $translate.instant('language.english'),
-                        language: 'en',
-                        culture: 'en-gb',
-                        languageTranslate: 'en'
-                    },
-                    {
-                        LanguageId: 2,
-                        LanguageName: $translate.instant('language.chinese'),
-                        language: 'cn',
-                        culture: 'en-gb',
-                        languageTranslate: 'cn'
+        ['$translate', 'localStorageService',
+            function ($translate, localStorageService) {
+                var service = {},
+                    languageList = [
+                        {
+                            LanguageId: 1,
+                            LanguageName: $translate.instant('language.english'),
+                            language: 'en',
+                            culture: 'en-gb',
+                            languageTranslate: 'en',
+                            selected: false
+                        },
+                        {
+                            LanguageId: 2,
+                            LanguageName: $translate.instant('language.chinese'),
+                            language: 'cn',
+                            culture: 'en-gb',
+                            languageTranslate: 'cn',
+                            selected: false
+                        }
+                    ];
+
+
+                /*
+                 * get list
+                 */
+                service.getList = function () {
+                    var user = localStorageService.getUserInfo();
+                    if (user) {
+                        var languageId = user.languageId;
+                        this.getLanguageById(languageId);
                     }
-                ];
+                    return languageList;
+                };
 
-
-            /*
-             * get list
-             */
-            service.getList = function () {
-                return languageList;
-            };
-
-            /*
-             * get language by languageId
-             */
-            service.getLanguageById = function (Id) {
-                var len = languageList.length;
-                for (var i = 0; i < len; i++) {
-                    var language = languageList[i];
-                    if (language.LanguageId === Id) {
-                        return language;
+                /*
+                 * get language by languageId
+                 */
+                service.getLanguageById = function (Id) {
+                    var len = languageList.length;
+                    for (var i = 0; i < len; i++) {
+                        var language = languageList[i];
+                        if (language.LanguageId === Id) {
+                            language.selected = true;
+                            return language;
+                        }else{
+                            language.selected = false;
+                        }
                     }
-                }
-            };
+                };
 
-            /*
-             *  refresh the translate language
-             *  todo: hard code, need to rewrite
-             */
-            service.refreshLanguage = function () {
-                languageList[0].LanguageName = $translate.instant('language.english');
-                languageList[1].LanguageName = $translate.instant('language.chinese');
-            };
+                /*
+                 *  refresh the translate language
+                 *  todo: hard code, need to rewrite
+                 */
+                service.refreshLanguage = function () {
+                    languageList[0].LanguageName = $translate.instant('language.english');
+                    languageList[1].LanguageName = $translate.instant('language.chinese');
+                };
 
-            return service;
-        }]);
+                return service;
+            }]);
 })(angular);
