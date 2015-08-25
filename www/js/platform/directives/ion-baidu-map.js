b129c11d4dbb4e8bf4ebe5f31b89fcc987fa90e1
Jackey-Sparrow <1109952026@qq.com>
2015-08-25 22:32:32 +0800

    implement ion-baidu-map
    the simple demo

--- www/js/platform/directives/ion-baidu-map.js
+++ www/js/platform/directives/ion-baidu-map.js
@@ -59,7 +59,7 @@
                 };
             }]);
 
-    angular.module('ionic.extension').directive('appMap', function () {
+    angular.module('ionic.extension').directive('appMap', function ($timeout) {
         return {
             restrict: 'E',
             replace: true,
@@ -75,19 +75,24 @@
                 address: '@'
             },
             link: function (scope, element, attrs) {
-                var map;
-                // 百度地图API功能
-                map = new BMap.Map('allMap');
-                //map.addControl(new BMap.ZoomControl());
-                // 创建地址解析器实例
-                var myGeo = new BMap.Geocoder();
-                // 将地址解析结果显示在地图上,并调整地图视野
-                myGeo.getPoint(scope.address, function (point) {
-                    if (point) {
-                        map.centerAndZoom(point, 16);
-                        map.addOverlay(new BMap.Marker(point));
-                    }
-                }, '');
+                $timeout(function () {
+                    var map;
+                    // 百度地图API功能
+                    map = new BMap.Map('allMap');
+                    //map.addControl(new BMap.ZoomControl());
+                    // 创建地址解析器实例
+                    //var myGeo = new BMap.Geocoder();
+                    // 将地址解析结果显示在地图上,并调整地图视野
+                    //myGeo.getPoint(scope.address, function (point) {
+                    //    if (point) {
+                    //        map.centerAndZoom(point, 16);
+                    //        map.addOverlay(new BMap.Marker(point));
+                    //    }
+                    //}, '');
+                    var point = new BMap.Point(116.404, 39.915);
+                    map.centerAndZoom(point, 15);
+                });
+
             }
         };
     });
