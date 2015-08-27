c6d62eb48ab925cc712e231298446d030acc315f
Jackey-Sparrow <1109952026@qq.com>
2015-08-27 21:06:57 +0800

    add marker to baidu map

--- www/platform/directives/ion-baidu-map.js
+++ www/platform/directives/ion-baidu-map.js
@@ -52,48 +52,42 @@
             };
         }]);
     angular.module('ionic.extension').controller('ionBaiduMapController',
-        ['$scope',
-            function ($scope) {
+        ['$scope', '$timeout',
+            function ($scope, $timeout) {
                 $scope.back = function () {
                     $scope.ionMap.hide();
                 };
-            }]);
 
-    angular.module('ionic.extension').directive('appMap', function ($timeout) {
-        return {
-            restrict: 'E',
-            replace: true,
-            template: '<div id="allMap"></div>',
-            scope: {
-                center: '=',		// Center point on the map (e.g. <code>{ latitude: 10, longitude: 10 }</code>).
-                markers: '=',	   // Array of map markers (e.g. <code>[{ lat: 10, lon: 10, name: "hello" }]</code>).
-                width: '@',		 // Map width in pixels.
-                height: '@',		// Map height in pixels.
-                zoom: '@',		  // Zoom level (one is totally zoomed out, 25 is very much zoomed in).
-                zoomControl: '@',   // Whether to show a zoom control on the map.
-                scaleControl: '@',   // Whether to show scale control on the map.
-                address: '@'
-            },
-            link: function (scope, element, attrs) {
+
                 $timeout(function () {
-                    var map;
-                    // 百度地图API功能
-                    map = new BMap.Map('allMap');
-                    //map.addControl(new BMap.ZoomControl());
-                    // 创建地址解析器实例
-                    //var myGeo = new BMap.Geocoder();
-                    // 将地址解析结果显示在地图上,并调整地图视野
-                    //myGeo.getPoint(scope.address, function (point) {
-                    //    if (point) {
-                    //        map.centerAndZoom(point, 16);
-                    //        map.addOverlay(new BMap.Marker(point));
-                    //    }
-                    //}, '');
+                    //instance
+                    $scope.map = new BMap.Map('allMap');
+                    //init map and set the center
                     var point = new BMap.Point(116.404, 39.915);
-                    map.centerAndZoom(point, 15);
+                    $scope.map.centerAndZoom(point, 15);
+                    //map type control
+                    $scope.map.addControl(new BMap.MapTypeControl());
+                    //open zoom
+                    //map.enableScrollWheelZoom(true);
+
+                    var marker = new BMap.Marker(point);
+                    $scope.map.addOverlay(marker);
+                    marker.setAnimation(BMAP_ANIMATION_BOUNCE);
                 });
 
-            }
-        };
-    });
+                $scope.centerOnMe = function () {
+                    var geolocation = new BMap.Geolocation();
+                    geolocation.getCurrentPosition(function(r){
+                        if(this.getStatus() == BMAP_STATUS_SUCCESS){
+                            var mk = new BMap.Marker(r.point);
+                            $scope.map.addOverlay(mk);
+                            $scope.map.panTo(r.point);
+                            //alert('您的位置：'+r.point.lng+','+r.point.lat);
+                        }
+                        else {
+                            alert('failed'+this.getStatus());
+                        }
+                    },{enableHighAccuracy: true});
+                };
+            }]);
 })(angular);
