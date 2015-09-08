e1f2a035a9f8bba8f41e5b06e4d4b7e8707ca6f7
Jackey.Sparrow <1109952026@qq.com>
2015-09-08 19:42:17 +0800

    android tab

--- www/app.js
+++ www/app.js
@@ -16,7 +16,7 @@ var requireModules = [
 
 angular.module('hiApp', requireModules)
 
-    .run(function ($ionicPlatform,$rootScope) {
+    .run(function ($ionicPlatform, $rootScope) {
         $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
@@ -34,7 +34,21 @@ angular.module('hiApp', requireModules)
             console.log('401 http error ');
         });
 
-    }).config(function ($stateProvider, $urlRouterProvider) {
+    }).config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+        $ionicConfigProvider.platform.ios.tabs.style('standard');
+        $ionicConfigProvider.platform.ios.tabs.position('bottom');
+        $ionicConfigProvider.platform.android.tabs.style('standard');
+        $ionicConfigProvider.platform.android.tabs.position('standard');
+
+        $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+        $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+        $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+        $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+        $ionicConfigProvider.platform.ios.views.transition('ios');
+        $ionicConfigProvider.platform.android.views.transition('android');
 
         // Ionic uses AngularUI Router which uses the concept of states
         // Learn more here: https://github.com/angular-ui/ui-router
